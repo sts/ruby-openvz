9fcf0c97d299881907236bf5995950cfcff334b9
Stefan Schlesinger <sts@ono.at>
2012-10-30 19:43:24 +0100

    Removing skeleton for vendored libs

--- lib/openvz.rb
+++ lib/openvz.rb
@@ -6,7 +6,6 @@ module OpenVZ
     autoload :Log,         "openvz/log"
     autoload :Shell,       "openvz/shell"
     autoload :Inventory,   "openvz/inventory"
-    autoload :Vendor,      "openvz/vendor"
     autoload :Container,   "openvz/container"
     autoload :Util,        "openvz/util"
     autoload :ConfigHash,  "openvz/confighash"
