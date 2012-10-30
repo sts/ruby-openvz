f6670734bea0017a79b330ca911c4a9156acd635
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2012-10-30 22:44:33 +0530

    Bump version

--- lib/openvz.rb
+++ lib/openvz.rb
@@ -11,7 +11,7 @@ module OpenVZ
     autoload :Util,        "openvz/util"
     autoload :ConfigHash,  "openvz/confighash"
 
-    VERSION = "1.5.3"
+    VERSION = "1.5.4"
     
     def self.version
         VERSION
